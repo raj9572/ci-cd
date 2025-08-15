f78898c08792810a25dc9e037cc9b9e983b28a89
rajali <rajali1432@gmail.com>
2025-08-16 00:17:33 +0530

    make import to commonjs

--- _tests_/math.test.js
+++ _tests_/math.test.js
@@ -1,4 +1,4 @@
-import { sum } from "../math.js";
+const { sum } = require("../math.js");
 
 test("adds 2 + 3 to equal 5 (PASS)", () => {
   expect(sum(2, 3)).toBe(5); // ✅ This will pass
--- math.js
+++ math.js
@@ -1,4 +1,4 @@
-// math.js
-export function sum(a, b) {
+function sum(a, b) {
   return a + b;
 }
+module.exports = { sum };
