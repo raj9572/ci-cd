d783f564aa556cafe5f7c99e859a0b0e4ec74005
rajali <rajali1432@gmail.com>
2025-08-16 00:21:57 +0530

    pass the all test

--- _tests_/math.test.js
+++ _tests_/math.test.js
@@ -4,6 +4,7 @@ test("adds 2 + 3 to equal 5 (PASS)", () => {
   expect(sum(2, 3)).toBe(5); // ✅ This will pass
 });
 
-test("adds 5 + 5 to equal 20 (FAIL)", () => {
-  expect(sum(5, 5)).toBe(20); // ❌ This will fail on purpose
+test("adds 5 + 5 to equal 10 (PASS)", () => {
+  expect(sum(5, 5)).toBe(10);
 });
+
