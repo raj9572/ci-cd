7146c055690dbaf465d009058b290126709dd7af
rajali <rajali1432@gmail.com>
2025-08-16 00:04:59 +0530

    Day 4: add tests

---
+++ _tests_/math.test.js
@@ -0,0 +1,9 @@
+import { sum } from "../math.js";
+
+test("adds 2 + 3 to equal 5 (PASS)", () => {
+  expect(sum(2, 3)).toBe(5); // ✅ This will pass
+});
+
+test("adds 5 + 5 to equal 20 (FAIL)", () => {
+  expect(sum(5, 5)).toBe(20); // ❌ This will fail on purpose
+});
