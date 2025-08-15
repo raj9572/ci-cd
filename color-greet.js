ed1e832cafbf82c34f7ccdd6acec79012659e828
rajali <rajali1432@gmail.com>
2025-08-15 23:40:12 +0530

    Day 3: use chalk in CI

---
+++ color-greet.js
@@ -0,0 +1,6 @@
+// color-greet.js
+import chalk from "chalk"; // ES Module import
+
+console.log(chalk.green("Hello, Raj! Your CI/CD pipeline is working 🎉"));
+console.log(chalk.blue("This text is blue!"));
+console.log(chalk.red("This text is red!"));
